e9e985caf0df9d37a07a343e1c6b3d6c4e82f428
Albert Roldugin <a.roldugin@initlab.ru>
2026-02-18 19:51:55 +0500

    optimization cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,19 @@
-import { defineConfig } from 'cypress';
+import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'https://pokemonbattle.ru/login',
-    supportFile: 'cypress/support/e2e.js',
-    specPattern: 'cypress/e2e/**/*.cy.{js,ts}',
+    baseUrl: "https://pokemonbattle.ru/login",
+    supportFile: "cypress/support/e2e.js",
+    specPattern: "cypress/e2e/**/*.cy.{js,ts}",
+    /**
+     * Позволяет взаимодействовать с процессом Node.js.
+     * Используется для регистрации задач (tasks), перехвата событий браузера
+     * и динамического изменения переменных окружения.
+     * @param {function} on - функция, которая будет вызываться при каждом событии node процесса
+     * @param {object} config - Cypress конфигурация
+     */
     setupNodeEvents(on, config) {
-      // Настройка событий node
+      // implement node event listeners here
     },
   },
   env: {
