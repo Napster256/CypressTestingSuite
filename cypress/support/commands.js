c6c83205b82eb820911a0e90cd4b682974377731
Albert Roldugin <a.roldugin@initlab.ru>
2026-02-20 14:29:18 +0500

    add Readme for other folders

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,7 @@
+// кастомные команды, которых нет в Cypress
+
+
+
 // ***********************************************
 // This example commands.js shows you how to
 // create various custom commands and overwrite
